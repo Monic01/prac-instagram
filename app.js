9cdbeb49206442c56052628819bb92a5fb18eafc
Monic01 <eudemonic01@gmail.com>
2022-05-16 16:27:28 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+document.querySelector("#join").addEventListener("click", function () {
+  document.querySelector(".login-container").classList.toggle("hidden");
+  document.querySelector(".join-container").classList.toggle("hidden");
+});
+
+document.querySelector("#login").addEventListener("click", function () {
+  document.querySelector(".login-container").classList.toggle("hidden");
+  document.querySelector(".join-container").classList.toggle("hidden");
+});
